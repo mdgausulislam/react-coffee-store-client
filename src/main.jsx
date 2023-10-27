777003fcd0034f4b09f6d49af8c6563d505f83df
unknown <gausulislam5@gmail.com>
2023-10-27 12:42:56 +0600

    update componets and router setup

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,22 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import AddCoffee from './components/AddCoffee.jsx';
+import UpdateCoffee from './components/UpdateCoffee.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <App></App>,
   },
+  {
+    path:'addCoffee',
+    element:<AddCoffee></AddCoffee>
+  },
+  {
+    path:'updateCoffee',
+    element:<UpdateCoffee></UpdateCoffee>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
