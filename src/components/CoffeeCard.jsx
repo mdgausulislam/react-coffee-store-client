bb89fddc0ae4cd37f73b26754f48d446a206fd9f
unknown <gausulislam5@gmail.com>
2023-10-30 00:59:32 +0600

    update loader in prams dtabase update

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import UpdateCoffee from './UpdateCoffee';
 
 const CoffeeCard = ({ coffee }) => {
-    const { name, quantity, supplier, taste, category, details, photo } = coffee;
+    const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
+
+
+    const handleDelete = _id => {
+        console.log(_id);
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your coffee has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+
+    }
     return (
         <div className="card card-side bg-base-100 shadow-xl">
             <figure><img src={photo} alt="Movie" /></figure>
@@ -15,8 +52,10 @@ const CoffeeCard = ({ coffee }) => {
                 <div className="card-actions justify-end">
                     <div className="btn-group btn-group-vertical space-y-4">
                         <button className="btn btn-active">View</button>
-                        <button className="btn">Edit</button>
-                        <button className="btn">Delete</button>
+                        <Link to={`UpdateCoffee/${_id}`}>
+                            <button className="btn">Edit</button>
+                        </Link>
+                        <button onClick={() => handleDelete(_id)} className="btn bg-orange-500">Delete</button>
                     </div>
                 </div>
             </div>
